9ddfe27ab5ffa42d54677519dc704a960dcb8141
JK Gunnink <jgunnink@gmail.com>
2015-02-21 18:51:09 +0800

    Added ability to remove avatar from user profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,6 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.for(:account_update) << :name
    devise_parameter_sanitizer.for(:sign_up) << :avatar
    devise_parameter_sanitizer.for(:account_update) << :avatar
+   devise_parameter_sanitizer.for(:account_update) << :remove_avatar
  end
 end
