a5975fe2a87f37aedc5cc55068dc6e415232a197
JK Gunnink <jgunnink@gmail.com>
2016-02-07 21:58:55 +0800

    Changed tables to add players to scorecards

--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -1,4 +1,5 @@
 class Scorecard < ActiveRecord::Base
 	belongs_to :user
   validates :description, presence: true
+  has_many :players
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308114459) do
+ActiveRecord::Schema.define(version: 20160207132707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "player_name"
+  end
 
   create_table "scorecards", force: :cascade do |t|
     t.string   "description"
